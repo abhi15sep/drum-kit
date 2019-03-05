326ac1dad4200c5c592c26f6a81bab7936655a5d
Abhishek K <abhishekakade@gmail.com>
2019-03-05 23:48:20 +0530

    fix: added sound files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,54 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Drum Kit</h1>
+        <div className="container">
+          <div id="drum-machine">
+            <div data-key="65" className="drum-pad">
+              <kbd>A</kbd>
+              <span className="sound">clap</span>
+            </div>
+
+            <div data-key="83" className="drum-pad">
+              <kbd>S</kbd>
+              <span className="sound">hihat</span>
+            </div>
+
+            <div data-key="68" className="drum-pad">
+              <kbd>D</kbd>
+              <span className="sound">kick</span>
+            </div>
+
+            <div data-key="70" className="drum-pad">
+              <kbd>F</kbd>
+              <span className="sound">openhat</span>
+            </div>
+
+            <div data-key="71" className="drum-pad">
+              <kbd>G</kbd>
+              <span className="sound">boom</span>
+            </div>
+
+            <div data-key="72" className="drum-pad">
+              <kbd>H</kbd>
+              <span className="sound">ride</span>
+            </div>
+
+            <div data-key="74" className="drum-pad">
+              <kbd>J</kbd>
+              <span className="sound">snare</span>
+            </div>
+
+            <div data-key="75" className="drum-pad">
+              <kbd>K</kbd>
+              <span className="sound">tom</span>
+            </div>
+
+            <div data-key="76" className="drum-pad">
+              <kbd>L</kbd>
+              <span className="sound">tink</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
