a5a6a199e99084e115e76165bc002035cd4b286e
Abhishek K <abhishekakade@gmail.com>
2019-03-18 22:25:12 +0530

    Updates

---
+++ precache-manifest.36ba697e91a7a0538dc8964747d320ef.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d5e5a8aa8dcb1a6f3c247d685ec4ebb",
+    "url": "/drum-kit/static/media/x.4d5e5a8a.mp3"
+  },
+  {
+    "revision": "abe3d2806266425f58ac",
+    "url": "/drum-kit/static/css/main.fa570a86.chunk.css"
+  },
+  {
+    "revision": "85a63a1f0769e337a5ea",
+    "url": "/drum-kit/static/js/runtime~main.85a63a1f.js"
+  },
+  {
+    "revision": "e8253c34dd78cd76d6a4",
+    "url": "/drum-kit/static/js/2.e8253c34.chunk.js"
+  },
+  {
+    "revision": "f10ba3d1c090e49d35ca5294331b68db",
+    "url": "/drum-kit/static/media/q.f10ba3d1.wav"
+  },
+  {
+    "revision": "7a90c58ff284580ec7b1336e1d700adf",
+    "url": "/drum-kit/static/media/w.7a90c58f.wav"
+  },
+  {
+    "revision": "b29e205797c61288f00c4ac4c30d92d8",
+    "url": "/drum-kit/static/media/e.b29e2057.wav"
+  },
+  {
+    "revision": "3f9fdf5d9386ed822c55dc86c294c6a8",
+    "url": "/drum-kit/static/media/a.3f9fdf5d.wav"
+  },
+  {
+    "revision": "a55ed0c560fb8e9be2b956064a149553",
+    "url": "/drum-kit/static/media/s.a55ed0c5.wav"
+  },
+  {
+    "revision": "5482e8be57bfa9085210f6fe3055790f",
+    "url": "/drum-kit/static/media/d.5482e8be.wav"
+  },
+  {
+    "revision": "9eb8d6f7f163403ceb2160366876e76c",
+    "url": "/drum-kit/static/media/z.9eb8d6f7.wav"
+  },
+  {
+    "revision": "abe3d2806266425f58ac",
+    "url": "/drum-kit/static/js/main.abe3d280.chunk.js"
+  },
+  {
+    "revision": "3c8ca9470540bbbea8eaa187bfe82a39",
+    "url": "/drum-kit/static/media/c.3c8ca947.mp3"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-kit/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-kit/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-kit/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-kit/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-kit/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-kit/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-kit/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-kit/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-kit/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "78aae14e803938d06da80e25ef10205a",
+    "url": "/drum-kit/index.html"
+  }
+];
